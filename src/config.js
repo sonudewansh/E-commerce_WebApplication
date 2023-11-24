807f028c67f9130448bd061889a6a459ab0c2133
notebooksfield <sonudewansh@gmail.com>
2023-11-24 22:08:53 +0530

    Update src/config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const LOCALHOST = "http://localhost:5173/"
-const PROD ="https://notebooksfield.vercel.app/"
-const URL = `${LOCALHOST}`;
+// const PROD ="https://notebooksfield.vercel.app/"
+const PROD ="https://ecommerce-nine-theta-70.vercel.app/"
+const URL = `${PROD}`;
 
 const LOGIN = () => URL + 'api/login';
 const SIGNUP =  URL + 'api/sign-up';
