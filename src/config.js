d69033c3a89dfe0fac48c8a6ac2426db884ef65c
notebooksfield <sonudewansh@gmail.com>
2023-11-24 22:53:01 +0530

    Update src/db/dbClient.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 const LOCALHOST = "http://localhost:5173/"
-// const PROD ="https://notebooksfield.vercel.app/"
+// const PROD = "https://todoauth11.vercel.app/"
 const PROD ="https://ecommerce-nine-theta-70.vercel.app/"
 const URL = `${PROD}`;
 
 const LOGIN = () => URL + 'api/login';
 const SIGNUP =  URL + 'api/sign-up';
 
-
-
 export { LOGIN, SIGNUP}
+
