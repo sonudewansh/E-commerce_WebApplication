6a10dbfb5b74c6d3b42c12d017b7acaa56d2c42a
notebooksfield <sonudewansh@gmail.com>
2023-11-25 00:25:57 +0530

    new 1 commitssS

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const LOCALHOST = "http://localhost:5173/"
 // const PROD = "https://todoauth11.vercel.app/"
-const PROD ="https://ecommerce-sonudewanshs-projects.vercel.app/"
+const PROD ="https://buy-laptops.vercel.app/"
 export const URL = `${PROD}`;
 
 const LOGIN = () => URL + 'api/login';
